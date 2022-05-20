37da7959a16950515f039a1e92330ef69fc64e52
Jian Andrievski <jian.andrievski@vinted.com>
2022-05-20 15:13:59 +0300

    Fixed missing line in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "wordly#index"
 
   get "/wordly", to: "wordly#index"
-    
+  get "/check_word", to: "check_word#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
