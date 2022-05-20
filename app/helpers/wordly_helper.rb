45445d7a093164827043743cc33ca8780fd6d693
Aiste Tviskute <aiste.tviskute@vinted.com>
2022-05-20 09:45:22 +0300

    Start of the project

---
+++ app/helpers/wordly_helper.rb
@@ -0,0 +1,2 @@
+module WordlyHelper
+end
---
+++ test/controllers/wordly_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class WordlyControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
