a9469e0ec4d0cdd672181a16d847b5c18de71109
Jian Andrievski <jian.andrievski@vinted.com>
2022-08-11 09:42:07 +0300

    fixed routes for logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
 
   get "/wordly", to: "wordly#index"
   get "/check_word", to: "check_word#index"
+  
+  get "/logout", to: "users#destroy"
 
-  get "/users", to: "users#index"
-  post "/users", to: "users#create"
-  delete "/users", to: "users#destroy"
+  resources :users
 
+  #get "/users", to: "users#index"
+  #post "/users", to: "users#create"
+  #delete "/users", to: "users#destroy"
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
