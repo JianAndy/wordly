6d5b2f677fb2f5ac8b156ba76ef132c96e9461b2
Jian Andrievski <jian.andrievski@vinted.com>
2022-08-12 14:40:04 +0300

    show games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,10 @@
 class GamesController < ApplicationController
+    def index
+        @user_games = Game.where(user_id: session[:user_id])
+        redirect_to check_word_path
+    end
+
+
+
+
 end
