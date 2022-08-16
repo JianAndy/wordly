054bd48ac4a055f7d6859efa056f837eaad122bd
Jian Andrievski <jian.andrievski@vinted.com>
2022-08-16 23:03:00 +0300

    Added scorecard and changed next game logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,42 @@
 class GamesController < ApplicationController
     def index
-        @user_games = Game.where(user_id: session[:user_id])
-        redirect_to check_word_path
+        @games = games 
+        @personal_stats = personal_stats
+        #redirect_to games_path
+
+    end
+
+    def games 
+        Game.where(user_id: session[:user_id])
+    end 
+
+    def personal_stats
+
+        if @games.empty?
+            "No game stats"
+        elsif 
+            @games_count = @games.length
+            @games_won =  Game.where(user_id: session[:user_id], status: true)
+            @win_ratio =  (@games_won.length * 100) / @games_count
+
+        {
+            "Played" => @games_count,
+            "Win %" => @win_ratio,
+            "Current Streak" => '',
+            "Max streak" => '' , 
+            "Row Distribution" => {
+                1 => @games_won.where(row: 1).length,
+                2 => @games_won.where(row: 2).length,
+                3 => @games_won.where(row: 3).length,
+                4 => @games_won.where(row: 4).length,
+                5 => @games_won.where(row: 5).length,
+                6 => @games_won.where(row: 6).length
+            }
+        }
+    
+        end
+
+        
     end
 
 
